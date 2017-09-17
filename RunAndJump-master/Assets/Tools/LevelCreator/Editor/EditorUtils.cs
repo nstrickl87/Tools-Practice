2f79234e9b28a3919125df18ee039d8ef4fd917a
kilvaari <nstrickland@outlook.com>
2017-09-16 21:20:39 -0700

    Initial Project Commit
    Added Run and Jump Tutorial Project.

---
+++ RunAndJump-master/Assets/Tools/LevelCreator/Editor/EditorUtils.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+
+
+namespace RunAndJump.LevelCreator
+{
+    public static class EditorUtils
+    {
+        //Create a New Scene for Use
+        public static void NewScene()
+        {
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+            EditorSceneManager.NewScene(NewSceneSetup.EmptyScene);
+        }
+
+        public static void NewLevel()
+        {
+            NewScene();
+
+            //Create Level GameObject and assign the Level Component. 
+            GameObject levelGO = new GameObject("Level");
+            levelGO.transform.position = Vector3.zero;
+            levelGO.AddComponent<Level>();
+        }
+    }
+}
---
+++ RunAndJump-master/Assets/Tools/LevelCreator/Editor/MenuItems.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+
+namespace RunAndJump.LevelCreator {
+    public static class MenuItems {
+
+        [MenuItem("Tools/Level Creator/New Level Scene %L")]
+        private static void NewLevel()
+        {
+            EditorUtils.NewLevel();
+        }
+
+    }
+}
